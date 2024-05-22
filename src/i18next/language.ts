49da1c6a7850c935563be600c8aaf6730e797d1f
“mehrojalieev” <aliyevmehrojbek8006@gmail.com>
2024-05-22 16:31:43 +0500

    Bug fixed

--- src/i18next/language.ts
+++ src/i18next/language.ts
@@ -1,26 +1,25 @@
-import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next'
-import Backend from 'i18next-http-backend'
-import LanguageDetector from 'i18next-browser-languagedetector'
-const Languages: any = ['uz', 'ru', 'en']
+// i18n.ts
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
+// const Languages = ['uz', 'ru', 'en'];
 
-i18n.use(Backend)
-i18n.use(LanguageDetector)
-i18n.use(initReactI18next)
-i18n.init({
+i18n.use(Backend).use(LanguageDetector).use(initReactI18next)
+.init({
     fallbackLng: 'uz',
     debug: false,
-    whitelist: Languages,
+    // whitelist: Languages,
     interpolation: {
-        escapeValue: false
+      escapeValue: false
     },
-    delection: {
-        order: ['cookie', 'localStorage'],
-        lookupCookie: 'lang',
-        lookupLocalStorage: 'lang',
-        caches: ['cookie', 'localStorage']
+    detection: {
+      order: ['cookie', 'localStorage'],
+      lookupCookie: 'lang',
+      lookupLocalStorage: 'lang',
+      caches: ['cookie', 'localStorage']
     }
-})
+  });
 
-export default i18n
+export default i18n;
